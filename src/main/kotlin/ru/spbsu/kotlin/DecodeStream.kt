25b728421e8fc9b24c092962028c4b0b83d5d500
Maxim Khabarov <43231178+haboh@users.noreply.github.com>
2022-02-25 20:46:27 +0500

    done

--- src/main/kotlin/ru/spbsu/kotlin/DecodeStream.kt
+++ src/main/kotlin/ru/spbsu/kotlin/DecodeStream.kt
@@ -1,10 +1,56 @@
 package ru.spbsu.kotlin
 
 import java.io.File
+import java.io.IOException
 import java.io.InputStream
+import java.util.*
+import kotlin.experimental.xor
 
 class DecodeStream(rootFolder: File) : InputStream() {
+    private var posInMessage = 0
+    private var password: List<Int> = arrayListOf()
+    private var message: ByteArray = byteArrayOf()
+
+    init {
+        val order = generateCorrectFileOrder(rootFolder)
+        readFilesInCorrectOrder(order)
+        password = password.takeWhile { it != 0 }
+    }
+
+    private fun rearrangeFolder(folder: File): List<File> {
+        return folder.walk()
+            .toList()
+            .filter { it.absolutePath.count { it == '/' } == folder.absolutePath.count { it == '/' } + 1 }
+            .sortedBy { it.name }
+    }
+
+    private fun generateCorrectFileOrder(folder: File): List<File> {
+        if (!folder.exists() || !folder.isDirectory) {
+            throw IllegalArgumentException("Error with folder $folder")
+        }
+        val (directories, files) = rearrangeFolder(folder).partition { it.isDirectory }
+        return directories.flatMap { directory -> generateCorrectFileOrder(directory) } + files
+    }
+
+    private fun readFilesInCorrectOrder(order: List<File>) {
+        order.forEach { file ->
+            password += Date(file.lastModified()).seconds % 10
+            message += file.readBytes()
+        }
+    }
+
     override fun read(): Int {
-        TODO()
+        if (posInMessage == message.size) {
+            return -1
+        }
+        if (password.isEmpty()) {
+            throw IllegalArgumentException("Password is empty")
+        }
+        val curPasswordPosition = posInMessage % password.size
+        val curPasswordByte = password[curPasswordPosition].toByte()
+        val curMessageByte = message[posInMessage]
+        val curByte = curPasswordByte.xor(curMessageByte)
+        posInMessage++
+        return curByte.toInt()
     }
 }
--- src/main/kotlin/ru/spbsu/kotlin/EncodeStream.kt
+++ src/main/kotlin/ru/spbsu/kotlin/EncodeStream.kt
@@ -3,8 +3,38 @@ package ru.spbsu.kotlin
 import java.io.File
 import java.io.OutputStream
 
-class EncodeStream (private val rootFolder: File, private val password: String): OutputStream() {
+
+class EncodeStream(private val rootFolder: File, private val password: String) : OutputStream() {
+    private val millisecondsInOnSecond = 1000L
+
+    init {
+        if (!rootFolder.exists()) {
+            throw IllegalArgumentException("${rootFolder.name} doesn't exists")
+        }
+        if (password.toIntOrNull() == null || password.contains("0") || password.isEmpty()) {
+            throw IllegalArgumentException("Error password $password")
+        }
+    }
+
+    private val data = mutableListOf<Byte>()
+    private var passwordIndex = 0
+
     override fun write(b: Int) {
-        TODO()
+        val currentPasswordByte = password[passwordIndex].code - '0'.code
+        data.add(b.xor(currentPasswordByte).toByte())
+        passwordIndex = (passwordIndex + 1) % password.length
+    }
+
+    override fun close() {
+        val files = password.indices
+            .map { File(rootFolder, it.toString()) }
+            .sortedBy { it.name }
+        files[0].writeBytes(data.toByteArray())
+        files.indices.forEach { i ->
+            files[i].createNewFile()
+            val passwordPos = i % password.length
+            val passwordByte = password[passwordPos].code - '0'.code
+            files[i].setLastModified(passwordByte * millisecondsInOnSecond)
+        }
     }
 }
